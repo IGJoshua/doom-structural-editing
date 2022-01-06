9ce1693ef41cec59862c34e2cb196d3427e1a96b
Joshua Suskalo <joshua@iris.tv>
2022-01-06 17:39:18 -0600

    Add cleverparens and paxedit

---
+++ config.el
@@ -0,0 +1,33 @@
+;;; editor/structural-editing/config.el -*- lexical-binding: t; -*-
+
+(use-package! evil-cleverparens
+  :when (featurep! :editor evil)
+  :hook ((lisp-mode . evil-cleverparens-mode)
+         (emacs-lisp-mode . evil-cleverparens-mode)
+         (ielm-mode . evil-cleverparens-mode)
+         (scheme-mode . evil-cleverparens-mode)
+         (racket-mode . evil-cleverparens-mode)
+         (hy-mode . evil-cleverparens-mode)
+         (lfe-mode . evil-cleverparens-mode)
+         (dune-mode . evil-cleverparens-mode)
+         (clojure-mode . evil-cleverparens-mode)
+         (fennel-mode . evil-cleverparens-mode)))
+
+(use-package! paredit
+  :unless (featurep! :editor lispy))
+
+(after! paredit
+  (use-package! paxedit
+    :unless (featurep! :editor lispy)
+    :hook ((lisp-mode . paxedit-mode)
+           (emacs-lisp-mode . paxedit-mode)
+           (ielm-mode . paxedit-mode)
+           (scheme-mode . paxedit-mode)
+           (racket-mode . paxedit-mode)
+           (hy-mode . paxedit-mode)
+           (lfe-mode . paxedit-mode)
+           (dune-mode . paxedit-mode)
+           (clojure-mode . paxedit-mode)
+           (fennel-mode . paxedit-mode))
+    :config
+    (add-hook! eval-expression-minibuffer-setup-hook #'paxedit-mode)))
---
+++ packages.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; -*-
+;;; editor/structural-editing/packages.el
+
+(when (featurep! :editor evil)
+  (package! evil-cleverparens :pin "8c45879d49bfa6d4e414b6c1df700a4a51cbb869")
+
+  (when (featurep! :editor lispy)
+    (package! lispyville :disable t)))
+
+(unless (featurep! :editor lispy)
+  (when (package! paredit :pin "8330a41e8188fe18d3fa805bb9aa529f015318e8")
+    (package! paxedit :pin "09f3d5aeb108937a801e77ef413e29eaa4ecc4be")))
